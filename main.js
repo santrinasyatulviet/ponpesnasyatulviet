3b88e6d2c9d9d8f50d458f2196c5c27a0c63e176
santrinasyatulviet <158811683+santrinasyatulviet@users.noreply.github.com>
2024-07-16 19:23:00 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+
+const klik1 = document.getElementById('btn1')
+const klik2 = document.getElementById('btn2')
+const klik3 = document.getElementById('btn3')
+const klik4 = document.getElementById('btn4')
+const klik5 = document.getElementById('btn5')
+const klik6 = document.getElementById('btn6')
+
+
+ klik1.style.color = 'gold'
+
+
+
+if (klik1 === btn1) {
+    function gantiwarna1() {
+        klik1.style.color = 'gold'
+        klik2.style.color = 'white'
+        klik3.style.color = 'white'
+        klik4.style.color = 'white'
+        klik5.style.color = 'white'
+         klik6.style.color = 'white'
+    
+    }
+    
+}
+if (klik2 === btn2) {
+    function gantiwarna2() {
+        klik1.style.color = 'white'
+        klik2.style.color = 'gold'
+        klik3.style.color = 'white'
+        klik4.style.color = 'white'
+        klik5.style.color = 'white'
+        klik6.style.color = 'white'
+        
+    
+    }
+    
+}
+if (klik3 === btn3) {
+    function gantiwarna3() {
+        klik1.style. color = 'white'
+        klik2.style. color = 'white'
+        klik3.style. color = 'gold'
+        klik4.style.color = 'white'
+        klik5.style.color = 'white'
+        klik6.style.color = 'white'
+        
+        
+    
+    }
+    
+}
+if (klik4 === btn4) {
+    function gantiwarna4() {
+        klik1.style. color = 'white'
+        klik2.style. color = 'white'
+        klik3.style. color = 'white'
+        klik4.style.color = 'gold'
+        klik5.style.color = 'white'
+        klik6.style.color = 'white'
+        
+        
+    
+    }
+    
+}
+if (klik5 === btn5) {
+    function gantiwarna5() {
+        klik1.style. color = 'white'
+        klik2.style. color = 'white'
+        klik3.style. color = 'white'
+        klik4.style.color = 'white'
+        klik5.style.color = 'gold'
+        klik6.style.color = 'white'
+
+        
+    
+    }
+    
+}
+if (klik6 === btn6) {
+    function gantiwarna6() {
+        klik1.style. color = 'white'
+        klik2.style. color = 'white'
+        klik3.style. color = 'white'
+        klik4.style.color = 'white'
+        klik5.style.color = 'white'
+        klik6.style.color = 'gold'
+        
+    
+    }
+    
+}
